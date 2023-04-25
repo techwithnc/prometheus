3e64fa7ed5350c465fcfe6fa3187edd7541fb48a
Tech With NC <120474799+techwithnc@users.noreply.github.com>
2023-04-25 15:48:10 +0630

    update prometheus download link.

--- how-to-install-prometheus.sh
+++ how-to-install-prometheus.sh
@@ -11,27 +11,27 @@ sudo chown prometheus:prometheus /etc/prometheus
 sudo chown prometheus:prometheus /var/lib/prometheus
 
 # Step 2 - Downloading Prometheus
-wget https://github.com/prometheus/prometheus/releases/download/v2.40.6/prometheus-2.40.6.linux-amd64.tar.gz
-sudo tar xvf prometheus-2.40.6.linux-amd64.tar.gz
+wget https://github.com/prometheus/prometheus/releases/download/v2.43.0/prometheus-2.43.0.linux-amd64.tar.gz
+sudo tar xvf prometheus-2.43.0.linux-amd64.tar.gz
 
 # Copy binaries file
-sudo cp prometheus-2.40.6.linux-amd64/prometheus /usr/local/bin/
-sudo cp prometheus-2.40.6.linux-amd64/promtool /usr/local/bin/
+sudo cp prometheus-2.43.0.linux-amd64/prometheus /usr/local/bin/
+sudo cp prometheus-2.43.0.linux-amd64/promtool /usr/local/bin/
 
 # Set user and group ownership to prometheus user
 sudo chown prometheus:prometheus /usr/local/bin/prometheus
 sudo chown prometheus:prometheus /usr/local/bin/promtool
 
 # Copy console and console libraries
-sudo cp -r prometheus-2.40.6.linux-amd64/consoles /etc/prometheus
-sudo cp -r prometheus-2.40.6.linux-amd64/console_libraries /etc/prometheus
+sudo cp -r prometheus-2.43.0.linux-amd64/consoles /etc/prometheus
+sudo cp -r prometheus-2.43.0.linux-amd64/console_libraries /etc/prometheus
 
 # Set user and group ownership to prometheus user
 sudo chown -R prometheus:prometheus /etc/prometheus/consoles
 sudo chown -R prometheus:prometheus /etc/prometheus/console_libraries
 
 # Delete downloaded file
-sudo rm -rf prometheus-2.40.6.linux-amd64.tar.gz prometheus-2.40.6.linux-amd64
+sudo rm -rf prometheus-2.43.0.linux-amd64.tar.gz prometheus-2.40.6.linux-amd64
 
 # Step 3 - Configure Prometheus
 cat <<EOF | sudo tee /etc/prometheus/prometheus.yml
